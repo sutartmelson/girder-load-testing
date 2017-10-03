b3a1ea377e0bb3928f8e5862178cff928956b4d7
Stuart Nelson <stuart.nelson@kitware.com>
2017-10-03 13:47:04 -0400

    remove non locust files

--- scripts/locust.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pushd ../terraform > /dev/null
-
-LOCUST_MASTER_IP=$(terraform output -json | jq ".locust_master_ec2_address.value" | sed -e "s/\"//g")
-
-popd > /dev/null
-
-ssh -o StrictHostKeyChecking=no  -i $TF_VAR_key_path -l ubuntu $LOCUST_MASTER_IP $*
